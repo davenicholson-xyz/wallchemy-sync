6f0d931a6db84fce6245b2b5c05b5fc5e0748dfb
Dave Nicholson <me@davenicholson.xyz>
2025-06-03 20:39:38 +0100

    run command

--- main.go
+++ main.go
@@ -32,7 +32,12 @@ func main() {
 	app := app.NewApp(*port, indentifier)
 
 	udpHandler := func(msg string, src *net.UDPAddr) {
-		fmt.Printf("[UDP][%s] From %s: %s\n", indentifier, src.IP, msg)
+		cmd := exec.Command("wallchemy", "-fromsync", "-id", msg)
+		_, err := cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Printf("[UDP][%s] From %s: %s\n", indentifier, src.IP, msg)
 	}
 
 	udp := network.NewMulticastListener(app.Port, app.Identifier, udpHandler, false)
